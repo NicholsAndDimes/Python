bac420c2c5043f495addef7c816a52fda2f5624c
NicholsAndDimes <chris.n@blacksagetech.com>
2020-07-26 03:27:54 -0600

    stumped

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,14 +1,44 @@
+def play_game(player, ticdic_const):
+    while True:
+        choice = p_input(player)
+        player = game_replace(choice, player, ticdic_const)
+        game_continue = False
+        for k, v in ticdic.items():
+            if v.isdigit():
+                game_continue = True
+                break
+        if game_continue is False:
+            game_display()
+            break
+    print("Board filed up")
+    play_again(ticdic_const)
+
+
+def play_again(ticdic_const):
+    while True:
+        game_on = input("Would you like to play again? [Y/N]: ")
+        if game_on.lower() == "y" or game_on.lower() == "yes":
+            break
+        elif game_on.lower() == "n" or game_on.lower() == "no":
+            print("That was fun! Goodbye!")
+            quit()
+        print("Invalid input")
+    ticdic = ticdic_const
+    return ticdic
+
+
 def p_input(player):
     while True:
         game_display()
         print("Player {} turn".format(player))
-        choice = input("Please make a selection:")
+        choice = input("Please make a selection: ")
         if choice.isdigit():
             if int(choice) in range(1, 10):
                 if choice in ticdic[choice]:
-                    print(f"{choice} is in {ticdic[choice]}")
                     break
-        print("Invalid input")
+        print("""-----------------
+| Invalid input |
+-----------------""")
     return choice
 
 
@@ -28,28 +58,42 @@ def game_display():
              vert_line, ticdic['7'], ticdic['8'], ticdic['9'], vert_line))
 
 
-def game_replace(choice, player):
+def game_replace(choice, player, ticdic_const):
     ticdic[choice] = player
+    winning_conditions(ticdic_const)
     if player == "X":
         return "O"
     if player == "O":
         return "X"
 
 
-ticdic = {"1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9"}
-player = "X"
+def winning_conditions(ticdic_const):
+    players = ["X", "O"]
+    for p in players:
+        conditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+        for a in conditions:
+            game_continue = True
+            x = 0
+            for k, v in ticdic.items():
+                if int(k) in a:
+                    if v != p:
+                        game_continue = True
+                        continue
+                    else:
+                        x += 1
+                    if x == 3:
+                        game_continue = False
+                if game_continue is False:
+                    game_display()
+                    print(f"{p} has won 3 in a row, spots {a}")
+                    play_again(ticdic_const)
+
+
 vert_line = f"     |     |     "
 horz_line = f"-----+-----+-----"
-game_continue = True
+ticdic_const = {"1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9"}
 while True:
-    choice = p_input(player)
-    player = game_replace(choice, player)
-    game_continue = False
-    for k, v in ticdic.items():
-        if v.isdigit():
-            game_continue = True
-            break
-    if game_continue is False:
-        game_display()2
-        break
-print("Board filed up, winning conditions in next version")
+    ticdic = ticdic_const
+    game_continue = True
+    player = "X"
+    play_game(player, ticdic_const)
